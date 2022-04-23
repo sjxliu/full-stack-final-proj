00f842d06724975ba6c64596a21a295d673e7234
unknown <sjxliu8@gmail.com>
2022-04-23 00:50:12 -0500

    mobile sidebar base shows

--- sm_frontend/src/components/Sidebar.jsx
+++ sm_frontend/src/components/Sidebar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { RiHomeFill } from "react-icons/ri";
+import { IoIoArrowForward } from "react-icons/io";
 
-const Sidebar = () => {
+import logo from "../assets/volley-logo.png";
+
+const Sidebar = ({ user, closeToggle }) => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="flex flex-col justify0between bg-white h-full overfull-y-scrikk min-w-210 hide-scrollbar">
+      <div className="flex flex-col">
+        <Link to="/"
+        className="flex px-5 gap-2 my-6 pt-1 w-190 items-center"
+        >
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
