aa26e86fce8cebbc5ecca1a31be8b1132190b7c7
unknown <sjxliu8@gmail.com>
2022-05-07 12:46:12 -0500

    final touches

--- sm_frontend/src/App.js
+++ sm_frontend/src/App.js
@@ -2,12 +2,13 @@ import React, {useEffect} from "react";
 import {Routes, Route, useNavigate} from 'react-router-dom'
 import Login from "./components/Login";
 import { Home } from "./container/Home";
+import { fetchUser } from "./utilities/fetchUser";
 
 const App = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+    const User = fetchUser()
 
     if (!User) navigate('/login');
   }, []);
